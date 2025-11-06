ce245b335c6680e548a1f40c3c7e2c3efffa1e37
saikrishna21 <saikrishna.kosuri21@gmail.com>
2025-11-06 16:31:43 +0530

    Updated API base URL for Render backend

--- src/api.js
+++ src/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:5000"
+  baseURL: "https://ems-backend-r5bn.onrender.com"
 });
 
 api.interceptors.request.use((config) => {
